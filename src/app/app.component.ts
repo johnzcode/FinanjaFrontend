ba68a85dca3eb9e6a6e01709e5dd8986a0375aaa
John Aguirre <johnzcode@gmail.com>
2025-02-05 21:32:36 -0500

    agrego ruta personalizada para que al importar un componente el nombre no sea tan largo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from '@auth/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [LoginComponent],
+  template: ` <app-login /> `
 })
 export class AppComponent {
   title = 'FinanjaFrontend';
