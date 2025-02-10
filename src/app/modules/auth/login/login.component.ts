b3a204a91a88d9e9d4171dfc14831c6ae7cf2d8f
John Aguirre <johnzcode@gmail.com>
2025-02-09 21:24:51 -0500

    agrego etiqueta form para agregar el evento submit

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService, LoginResponse } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+    
+    loginForm!: FormGroup;
+    errorMsg: string = '';
 
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ){}
+
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', Validators.required]
+        });
+    }
+
+    onSubmit(): void {
+        if (this.loginForm) {
+          const credentials = {
+            emailOrUsername: this.loginForm.value.email,
+            password: this.loginForm.value.password
+          };
+    
+          this.authService.login(credentials).subscribe({
+            next: (res: LoginResponse) => {
+              
+              localStorage.setItem('token', res.token);
+              
+              this.router.navigate(['/dashboard']);
+            },
+            error: (err) => {
+              this.errorMsg = err.error.msg || 'Credenciales inválidas o error en el servidor';
+            }
+          });
+        }
+    }
 }
