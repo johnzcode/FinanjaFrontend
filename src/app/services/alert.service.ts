2d1006cd13c85916ef6549f14dffa5b9988beedf
John Aguirre <johnzcode@gmail.com>
2025-02-23 08:40:18 -0500

    instalo sweetalert e implemento servicio para ser usado

---
+++ src/app/services/alert.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  showAlert(
+    msg: string, 
+    title: string, 
+    icon: 'warning' | 'error' | 'info' | 'success' = 'warning',
+    confirmCallback?: () => void,
+    btnCancel: boolean = false
+  ) {
+    Swal.fire({
+        title: title,
+        html: msg,
+        icon: icon,
+        showCancelButton: btnCancel,
+        reverseButtons: true,
+        confirmButtonText: "Aceptar",
+        confirmButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar",
+        cancelButtonColor: "#d33"
+    }).then((result) => {
+        if (result.isConfirmed && confirmCallback) {
+            confirmCallback();
+        }
+    });
+  }
+}
